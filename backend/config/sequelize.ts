f619c682c36cf2044000d0f079ef2eb002664cf5
Caleb Scopetski <cscopetski@gmail.com>
2024-03-24 11:28:41 -0400

    add session cookie & exercise routes

--- backend/config/sequelize.ts
+++ backend/config/sequelize.ts
@@ -1,11 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../models/User.model';
 import { LoginCredential } from '../models/LoginCredential.model';
+import { Exercise } from '../models/Exercise.model';
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite',
-  models: [User, LoginCredential],
+  models: [User, LoginCredential, Exercise],
 });
 
 export default sequelize;
