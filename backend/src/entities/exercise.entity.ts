59a42b8ba50447f2f35c71ec65ffd74aa07c6466
Caleb Scopetski <cscopetski@gmail.com>
2024-04-14 14:01:02 -0400

    all tables created

--- backend/src/entities/exercise.entity.ts
+++ backend/src/entities/exercise.entity.ts
@@ -1,5 +1,7 @@
-import { Entity, PrimaryKey, Property, ManyToOne } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property, ManyToOne, Collection, OneToMany, Cascade } from '@mikro-orm/core';
 import { User } from './user.entity.js';
+import { SessionSet } from './set.entity.js';
+import { ChallengeSet } from './challenge-set.entity.js';
 
 @Entity()
 export class Exercise {
@@ -15,4 +17,10 @@ export class Exercise {
 
     @ManyToOne()
     user!: User;
+
+    @OneToMany(() => SessionSet, (session_set: SessionSet) => session_set.exercise, { cascade: [Cascade.ALL] })
+    sets = new Collection<SessionSet>(this);
+
+    @OneToMany(() => ChallengeSet, (challenge_set: ChallengeSet) => challenge_set.exercise, { cascade: [Cascade.ALL] })
+    challenge_sets = new Collection<ChallengeSet>(this);
 }
