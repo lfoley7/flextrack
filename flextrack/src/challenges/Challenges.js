3cb8f9285628396083eb4c87157ff4af0cac0298
Jagruti Chitte <jagruti2504@gmail.com>
2024-04-14 20:16:01 -0400

    Challenge page

--- flextrack/src/challenges/Challenges.js
+++ flextrack/src/challenges/Challenges.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import './Challenges.css';
 
-function Challenges(props) {
+/* function Challenges(props) {
     const navigate = useNavigate();
 
     return (
@@ -22,4 +22,46 @@ function Challenges(props) {
     )
 };
 
-export default Challenges;
+export default Challenges; */
+/*
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './ChallengeDetail.css'; // Ensure this is the correct path to your CSS file
+*/
+
+function Challenges() {
+    const navigate = useNavigate();
+
+    // You would fetch the actual challenges from your state or props
+    const challenges = [
+        { id: 1, name: 'MIKEYLIFTS204', description: 'Deadlift, Bench Pre...' },
+        { id: 2, name: 'JAGRUTIGAINZ333', description: 'Squat, Incline Dumbe...' },
+        // ... other challenges
+    ];
+
+    return (
+        <div className="challenge-layout">
+            <aside className="sidebar">
+                <h1>Challenges Home Page</h1>
+                <div className="challenge-list">
+                    {challenges.map(challenge => (
+                        <div key={challenge.id} className="challenge-item" onClick={() => navigate('/challenge/${challenge.id}')}>
+                            <div className="challenge-title">{challenge.name}</div>
+                            <div className="challenge-description">{challenge.description}</div>
+                        </div>
+                    ))}
+                </div>
+                <button className="new-challenge-btn" onClick={() => navigate('/new-challenge')}>New Challenge</button>
+            </aside>
+
+            <footer className="bottom-nav">
+                <span onClick={() => navigate('/home')}>H</span>
+                <span onClick={() => navigate('/challenges')}>CHL</span>
+                <span onClick={() => navigate('/friends')}>FRN</span>
+                <span onClick={() => navigate('/settings')}>SET</span>
+            </footer>
+        </div>
+    );
+}
+
+export default Challenges;
